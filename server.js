00e721ba6fba372f854ab51c2ac4f84f29a4fd2b
Dhivya <dhivyadharshini426@gmail.com>
2025-02-23 12:28:31 +0530

    Update MongoDB URI

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const questionRoutes = require("./routes/question.route");
 const contactRoutes = require("./routes/contact.route");
 const reportRoutes = require("./routes/report.route");
-const mongoose = require("./config/db");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,11 +14,22 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected Successfully! 🚀"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+// Routes
 app.use("/api/questions", questionRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/reports", reportRoutes);
 
-mongoose.connection.once("open", () => {
-  console.log("MongoDB connected");
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
